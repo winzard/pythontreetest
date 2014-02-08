aeff110355004e2d9199c980f124d82a521b8330
Maxim Vasenkov <max.vasenkov@gmail.com>
2014-02-08 17:21:05 +0400

    path fix

--- pythontreetest/settings.py
+++ pythontreetest/settings.py
@@ -1,4 +1,4 @@
-# Django settings for tree project.
+# Django settings for pythontreetest project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -115,14 +115,14 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'tree.urls'
+ROOT_URLCONF = 'pythontreetest.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'tree.wsgi.application'
+WSGI_APPLICATION = 'pythontreetest.wsgi.application'
 
 import os
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/'),
-                 os.path.join(os.path.dirname(__file__), '..', 'feincms/templates').replace('\\','/'),
+
 )
 
 INSTALLED_APPS = (
