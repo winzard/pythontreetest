c4f5c6c8789ff7b4847543f26ea7b9d631ab2ff8
Maxim Vasenkov <max.vasenkov@gmail.com>
2014-02-09 13:28:50 +0400

    упаковка

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bigtree',
-    version='0.2',
+    version='0.2.1',
     packages=['bigtree'],
     include_package_data=True,
     license='BSD License',  # example license
